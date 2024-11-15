19606d6365f4b76276ffdc67ca2103f295d4cb8f
BobelBanjamin <banjamin.bobel@edu.univ-fcomte.fr>
2024-11-15 13:27:21 +0300

    creation de la page des chats et importation de toute les images

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,10 @@ const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector(".menu");
 
 toggle.addEventListener("click", () => {
-  nav.ariaHidden = (nav.ariaHidden === "true") ? "false" : "true"; 
-  toggle.ariaExpanded = (toggle.ariaExpanded === "false") ? "true" : "false"; 
-  
-  if (toggle.ariaExpanded === "true") {
-    toggle.style.transform = "rotate(90deg)";
-  } else {
-    toggle.style.transform = "rotate(0deg)";
-  }
+  nav.ariaHidden = (nav.ariaHidden === "true") ? false : true;
 
-  document.body.classList.toggle("noscroll", toggle.ariaExpanded === "true");
+  toggle.ariaExpanded = (toggle.ariaExpanded === "false") ? true : false;
+
+  document.body.classList.toggle("noscroll", toggle.ariaExpanded);
 });
+
